5f7f1f503d3e5985fdb2791904a8225b5bb7765d
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-18 10:41:39 +0700

    menambahkan object data jenis kelamin

---
+++ app/src/main/java/com/example/questnavigasimvvm_169/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasimvvm_169.model
+
+import com.example.questnavigasimvvm_169.R
+
+object DataJK {
+    val isiJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
