495c44a489e8a49091feec569865bece8a68ac8e
Ryvqi <rifqikhabibi03@gmail.com>
2024-11-18 10:46:51 +0700

    menambahkan fungsi save data mahasiswa

--- app/src/main/java/com/example/questnavigasimvvm_169/ui/viewModel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/questnavigasimvvm_169/ui/viewModel/MahasiswaViewModel.kt
@@ -5,10 +5,21 @@ import com.example.questnavigasimvvm_169.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel: ViewModel() { // view model ": ViewModel"
     private val _uiState = MutableStateFlow(Mahasiswa())
     val uiState: StateFlow<Mahasiswa> = _uiState.asStateFlow()
 
-
+    fun saveDataMahasiswa(
+        ls :MutableList<String>
+    ){
+        _uiState.update { data ->
+            data.copy(  //ditampung dalam array []
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2]
+            )
+        }
+    }
 }
